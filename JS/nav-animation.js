f65b01a8dd025b4627a67de0b25e5e8f4120a6c4
Kartikey <ranjankartikey1999@gmail.com>
2020-01-08 04:32:13 +0530

    Reformatted code

--- JS/nav-animation.js
+++ JS/nav-animation.js
@@ -1,20 +1,21 @@
 const navSlide = function() {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
-  const body = document.querySelector('body');
+  const body = document.querySelector("body");
   const navLinks = document.querySelectorAll(".nav-links li");
 
   burger.addEventListener("click", () => {
     /*Toggle Nav*/
     nav.classList.toggle("nav-active");
+    /*  Toggle body*/
     body.classList.toggle("nav-body");
-    //Animate Links
+    /*    Animate links*/
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          0.3}s`;
+          0.5}s`;
       }
     });
     /*Burger Animation*/
